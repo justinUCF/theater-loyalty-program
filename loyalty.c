fc9bf7eb534200280d97c667a89a88add770615c
justin <gamboajustin03@gmail.com>
2024-04-01 12:21:21 -0400

    Update

--- loyalty.c
+++ loyalty.c
@@ -1,1 +1,25 @@
-ECHO is on.
+//Justin Gamboa
+//Dr. Neslisah Torosdagli
+//COP3502C Spring 2024
+//Theater Loyalty Program
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLEN 19
+
+typedef struct customer {
+    char name[MAXLEN+1];
+    int points;
+} customer;
+
+typedef struct treenode {
+    customer* cPtr;
+    int size;
+    struct treenode* left;
+    struct treenode* right;
+} treenode;
+
+int main(){
+    return 0;
+}
